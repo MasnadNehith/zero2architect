29dcec0ed317a1636e6af52ad024e2206f237cd8
MasnadNehith <masnadhossain@live.com>
2019-02-10 19:12:16 +0200

    changed server to port80

--- one_user/server.js
+++ one_user/server.js
@@ -1,6 +1,6 @@
 
 const http = require('http');
-const port = 3000;
+const port = 80;
 const hostname = process.env.dns_name;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
